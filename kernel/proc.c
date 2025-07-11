9ef87978d3af34339e06b92d6a06c17600c82ea0
saniaezzati <saniaezzati1384@gmail.com>
2025-07-11 13:51:43 +0330

    implement thread scheduling mechanism

--- kernel/proc.c
+++ kernel/proc.c
@@ -437,6 +437,68 @@ wait(uint64 addr)
   }
 }
 
+///////////// Thread scheduling functions /////////////
+
+struct thread *
+initthread(struct proc *p)
+{
+  if (!p->current_thread) {
+  for (int i = 0; i < NTHREAD; ++i) {
+  p->threads[i].trapframe = 0;
+  freethread(&p->threads[i]);
+  }
+  // Initialize main thread
+  struct thread *t = &p->threads[0];
+  t->id = p->pid;
+  if ((t->trapframe = (struct trapframe *)kalloc()) == 0) {
+  freethread(t);
+  return 0;
+  }
+  t->state = THREAD_RUNNING;
+  p->current_thread = t;
+  }
+  return p->current_thread;
+}
+
+int
+thread_schd(struct proc *p) {
+  if (!p->current_thread) {
+  return 1;
+  }
+  if (p->current_thread->state == THREAD_RUNNING) {
+  p->current_thread->state = THREAD_RUNNABLE;
+  }
+  acquire(&tickslock);
+  uint ticks0 = ticks;
+  release(&tickslock);
+  struct thread *next = 0;
+  struct thread *t = p->current_thread + 1;
+  for (int i = 0; i < NTHREAD; i++, t++) {
+  if (t >= p->threads + NTHREAD) {
+  t = p->threads;
+  }
+  if (t->state == THREAD_RUNNABLE) {
+  next = t;
+  break;
+  } else if (t->state == THREAD_SLEEPING && ticks0 - t->sleep_tick0 >= t->sleep_n) {
+  next = t;
+  break;
+  }
+  }
+  if (next == 0) {
+  return 0;
+  } else if (p->current_thread != next) {
+  next->state = THREAD_RUNNING;
+  struct thread *t = p->current_thread;
+  p->current_thread = next;
+  if (t->trapframe) {
+  *t->trapframe = *p->trapframe;
+  }
+  *p->trapframe = *next->trapframe;
+  }
+  return 1;
+}
+
 ///////////// Thread management functions /////////////
 
 struct thread *allocthread(uint64 start_thread, uint64 stack_address,
@@ -549,17 +611,18 @@ scheduler(void)
     for(p = proc; p < &proc[NPROC]; p++) {
       acquire(&p->lock);
       if(p->state == RUNNABLE) {
-        // Switch to chosen process.  It is the process's job
+        // Switch to chosen process. It is the process's job
         // to release its lock and then reacquire it
         // before jumping back to us.
-        p->state = RUNNING;
-        c->proc = p;
-        swtch(&c->context, &p->context);
-
-        // Process is done running for now.
-        // It should have changed its p->state before coming back.
-        c->proc = 0;
-        found = 1;
+        if (thread_schd(p)) {
+          p->state = RUNNING;
+          c->proc = p;
+          swtch(&c->context, &p->context);
+          // Process is done running for now.
+          // It should have changed its p->state before coming back.
+          c->proc = 0;
+          found = 1;
+        }
       }
       release(&p->lock);
     }
